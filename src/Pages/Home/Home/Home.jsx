8e5ec6dbd8fc7c1fd15c7c0adb2a6bb55b43852f
Junaid Hasan <spiderss907@gmail.com>
2023-08-16 18:16:41 +0600

    add chocolate button added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+import newChocolate from '../../../assets/images/chocolate-bar 1.svg'
+import ShowAllChocolates from "../ShowAllChocolates/ShowAllChocolates";
 
 
 const Home = () => {
     return (
-        <div>
-            <h3>this is home</h3>
+        <div className="max-w-[1112px] mx-auto mt-[72px]">
+            <Link className="">
+            <button className="flex gap-2 items-center mb-8 p-[17px] border rounded-lg text-xl font-medium text-[#141414d6]"><span className="">+ </span> New Chocolate <img src={newChocolate} alt="" /></button>
+            </Link>
+            <ShowAllChocolates></ShowAllChocolates>
         </div>
     );
 };
--- src/Pages/Shared/Banner/Banner.jsx
+++ src/Pages/Shared/Banner/Banner.jsx
@@ -3,7 +3,9 @@ import './Banner.css'
 const Banner = () => {
     return (
         <div className="max-w-[658px] banner mx-auto">
+            <div className=''>
             <h2 className="banner_text py-4 px-[65px]">Chocolate Management System</h2>
+            </div>
         </div>
     );
 };
